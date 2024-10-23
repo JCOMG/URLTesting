af856d300121b3f824bb481a64362fe5e6cc56a5
JCOMG <CXC1217@student.bham.ac.uk>
2024-10-23 20:32:11 +0100

    Unnit Testing

--- TransferAddress/app.py
+++ TransferAddress/app.py
@@ -1,23 +1,25 @@
 import string
 import random
-from flask import Flask, redirect, request, jsonify
+from flask import Flask, redirect, request, jsonify, render_template
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from flask import g
 from dotenv import load_dotenv
 import os
-
+import unittest
+import pymongo
 
 load_dotenv()
-
+# Mongo DB is a NoSQL database
 mongodb_uri = os.getenv('MONGODB_URI')
 print(f"MongoDB URI: {mongodb_uri}")
 
+
 def get_db():
     if 'db' not in g:
-        g.client = MongoClient(mongodb_uri, server_api=ServerApi('1'))
+        g.client = MongoClient(mongodb_uri)
         g.db = g.client['shortener_db']
-        g.collection = g.db['urls']
+        g.collection = g.db['urls']  # Similar to the Table in SQL
 
     return g.collection
 
@@ -49,7 +51,12 @@ def health():
 
 @app.route('/')
 def index():
-    return jsonify({"message": "Welcome ~"}), 200
+    return render_template('index.html')
+
+
+@app.route('/connect')
+def connect():
+    return jsonify({"message": "CONNECT SUCCESSFULLY"}), 200
 
 
 @app.route('/shorten', methods=['POST'])
@@ -64,6 +71,9 @@ def shorten_url():
         return jsonify({"error": "No URL provided"}), 400
 
     existing_entry = collection.find_one({"original_url": original_url})
+
+    if len(original_url) > 2000:
+        return jsonify({"message": "URL too long"}), 400
 
     if existing_entry:
         return jsonify({"short_url": request.host_url + existing_entry['short_url']})
@@ -86,6 +96,32 @@ def redirect_to_original(short_url):
         return redirect(url_entry['original_url'])
     else:
         return jsonify({"error": "URL not found"}), 404
+
+
+@app.route('/original', methods=['POST'])
+def get_original():
+    collection = get_db()
+    request_original = request.form.get('original')
+    custom_short = request.form.get('custom_short_url')
+
+    if not request_original:
+        return "Original URL is required", 400
+
+    if request_original:
+        if custom_short:
+            collection.insert_one({
+                'short_url': custom_short,
+                'original_url': request_original
+            })
+            short_url = custom_short  # as long as we press the url i y will auto go to the redirect_to_original function
+            return render_template('index.html', short_url=request.host_url + short_url)
+        else:
+            short_url = generate_short_url()
+            collection.insert_one({
+                'short_url': short_url,
+                'original_url': request_original
+            })
+        return render_template('index.html', short_url=request.host_url + short_url)
 
 
 if __name__ == "__main__":
